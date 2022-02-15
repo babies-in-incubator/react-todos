45622170eb4de5e458d81fb9781f6b12ff393d6e
rhee519 <rhee519@gmail.com>
2022-02-15 10:58:57 +0900

    ch03-02

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello, React Router DOM!</h1>
+      <Home />
+      <Topics />
+      <Contact />
+    </div>
+  );
+};
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Home</h2>Home...
+    </div>
+  );
+};
+
+const Topics = () => {
+  return (
+    <div>
+      <h2>Topics</h2>Topics...
+    </div>
+  );
+};
+
+const Contact = () => {
+  return (
+    <div>
+      <h2>Contact</h2>Contact...
+    </div>
+  );
+};
 
 export default App;
