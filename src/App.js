e66c1d290e0fee116ed8021010f0d3cdf9721f6b
rhee519 <rhee519@gmail.com>
2022-02-21 00:01:38 +0900

    ch05-init

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
-import Nav from "./Nav";
-import { useEffect, useState } from "react";
-import Article from "./Article";
-
-const Loading = () => <div>Now Loading...</div>;
+import React from "react";
 
 const App = () => {
-  const [article, setArticle] = useState({
-    id: 0,
-    title: "Welcome",
-    desc: "Hello, React & Ajax",
-  });
-  const [list, setList] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    fetch("list.json")
-      .then((res) => res.json())
-      .then((json) => setList(json))
-      .then(() => setLoading(false));
-  }, []);
-
   return (
     <div className="App">
-      <h1>WEB</h1>
-      {loading ? (
-        <Loading />
-      ) : (
-        <>
-          <Nav
-            list={list}
-            onClick={(id) => {
-              fetch(id + ".json")
-                .then((res) => res.json())
-                .then((json) => setArticle({ ...json }));
-            }}
-          />
-          <Article title={article.title} desc={article.desc} />
-        </>
-      )}
+      <h1>Root</h1>
     </div>
   );
 };
